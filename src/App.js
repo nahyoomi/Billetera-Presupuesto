8e464bb1f113e6d60e15b42eee50955d3b5f7657
Nahomi <nahyomi2000@gmail.com>
2022-06-06 18:26:30 -0500

    :tada:BUILD: New wallet project

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+import Pregunta from './Components/Pregunta';
+
+function App() {
+
+  // Definir presupuesto 
+  const [ presupuesto, guardarPresupuesto ] = useState(0);
+  const [ restante, guardarRestante ] = useState(0);
+  return (
+  <div className='container'>
+    <header>
+      <h1>Gasto Semanal</h1>
+      <div className='contenido-principal contenido'>
+        <Pregunta  
+          guardarPresupuesto = {guardarPresupuesto}
+          guardarRestante = {guardarRestante}
+        />
+      </div>
+      
+    </header>
+  </div>
+  );
+}
+
+export default App;
