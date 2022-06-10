c81f22e2c1cef7595ac8981cd953a012f47d7f3a
Nahomi <nahyomi2000@gmail.com>
2022-06-09 19:31:45 -0500

    :sparkles:Feat new formulary for expenses

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, {useState} from 'react';
 import Pregunta from './Components/Pregunta';
+import Formulario from './Components/Formulario';
 
 function App() {
 
   // Definir presupuesto 
   const [ presupuesto, guardarPresupuesto ] = useState(0);
   const [ restante, guardarRestante ] = useState(0);
+  const [ mostrarpregunta, actualizarPregunta ] = useState(true);
+
   return (
   <div className='container'>
     <header>
@@ -14,7 +17,17 @@ function App() {
         <Pregunta  
           guardarPresupuesto = {guardarPresupuesto}
           guardarRestante = {guardarRestante}
+          actualizarPregunta = {actualizarPregunta}
         />
+
+        <div className='row'>
+          <div className='one-half column'>
+            <Formulario />
+          </div>
+          <div className='one-half column'>
+            2
+          </div>
+        </div>
       </div>
       
     </header>
